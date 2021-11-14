6bfdd1cd25737509e1b85c6fb4cf1969bca0e665
Maher Wardak <maher.wardak@gmail.com>
2021-11-14 18:11:37 -0500

    Updated timeline

--- src/components/projects-timeline/projects-timeline.component.jsx
+++ src/components/projects-timeline/projects-timeline.component.jsx
@@ -7,8 +7,9 @@ import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import L_ExpenseTracker from "../../assets/img/projects/Expense_Tracker.webp";
 import L_StarWars from "../../assets/img/projects/starwars.webp";
-import L_Calculator from "../../assets/img/projects/Calculator.webp";
+import L_Calculator from "../../assets/img/projects/Calculator.jpg";
 import L_Chat from "../../assets/img/projects/Chat.webp";
+import L_Todo from "../../assets/img/projects/todo.webp";
 
 // skills
 import L_REACT from "../../assets/img/skills/react.svg";
@@ -58,7 +59,7 @@ const TimeLine = () => {
                         <ul className="list-styles pt-1">
                           <li>Register new user</li>
                           <li>Login exiting user</li>
-                          <li>Keep track of chat logs</li>
+                          <li>Chat with multiple users</li>
                         </ul>
                         <hr />
                         <strong>Tech used:</strong>
@@ -115,7 +116,10 @@ const TimeLine = () => {
                 </Accordion>
               </div>
               <div className="d-flex justify-content-between flex-nowrap text-center">
-                <UrlButton href="" target="_blank">
+                <UrlButton
+                  href="https://chat-zone-mw.herokuapp.com/ "
+                  target="_blank"
+                >
                   SEE LIVE
                 </UrlButton>
                 <UrlButton
@@ -127,12 +131,12 @@ const TimeLine = () => {
               </div>
             </div>
           </ImageEvent>
-          {/* Project: Expense Tracker */}
+          {/* Project: Star Wars API */}
           <ImageEvent
             className="text-center"
-            text="React Expense Tracker"
-            src={L_ExpenseTracker}
-            alt="React ToDo App"
+            text="Star Wars API"
+            src={L_StarWars}
+            alt="Star Wars API"
           >
             <div className="d-flex justify-content-between flex-column mt-1">
               <div>
@@ -148,16 +152,14 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong> This app was designed to
-                        track all expenses and help improve spending habits.
-                        Keeping a daily record of your expenses improves the
-                        financial health of your budget.
+                        <strong>Description:</strong> This app displays Star
+                        Wars character data. You can search by character or sift
+                        through pages. Each page display 10 charaters.
                         <hr />
                         <strong>Features:</strong>
                         <ul className="list-styles pt-1">
-                          <li>Add detailed transactions</li>
-                          <li>Remove transactions</li>
-                          <li>Respoisive Design</li>
+                          <li>Search characters by name</li>
+                          <li>Search characters by pages</li>
                         </ul>
                         <hr />
                         <strong>Tech used:</strong>
@@ -192,13 +194,13 @@ const TimeLine = () => {
               </div>
               <div className="d-flex justify-content-between flex-nowrap text-center">
                 <UrlButton
-                  href="https://expense-tracker-maher.herokuapp.com/"
+                  href="https://mw-starwars-api.herokuapp.com/"
                   target="_blank"
                 >
                   SEE LIVE
                 </UrlButton>
                 <UrlButton
-                  href="https://github.com/mwardak/Expense-Tracker"
+                  href="https://github.com/mwardak/Star-Wars-API"
                   target="_blank"
                 >
                   SOURCE CODE
@@ -207,12 +209,12 @@ const TimeLine = () => {
             </div>
           </ImageEvent>
 
-          {/* Project: Star Wars API */}
+          {/* Project: Task Manager */}
           <ImageEvent
             className="text-center"
-            text="Star Wars API"
-            src={L_StarWars}
-            alt="Star Wars API"
+            text="Task Manager"
+            src={L_Todo}
+            alt="Task Manager"
           >
             <div className="d-flex justify-content-between flex-column mt-1">
               <div>
@@ -228,14 +230,21 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong> This app displays Star
-                        Wars character data. You can search by character or sift
-                        through pages. Each page display 10 charaters.
+                        <strong>Description:</strong> This is task manager
+                        created for my team at work to help manage their daily
+                        tasks.
                         <hr />
                         <strong>Features:</strong>
                         <ul className="list-styles pt-1">
-                          <li>Search characters by name</li>
-                          <li>Search by pages</li>
+                          <li>Enter a task</li>
+                          <li>
+                            Cross out a completed task by selecting the checkbox
+                          </li>
+                          <li>Remove the crossed out task from the list</li>
+                          <li>
+                            Local storage implemented to store all incomplete
+                            tasks
+                          </li>
                         </ul>
                         <hr />
                         <strong>Tech used:</strong>
@@ -271,13 +280,13 @@ const TimeLine = () => {
               </div>
               <div className="d-flex justify-content-between flex-nowrap text-center">
                 <UrlButton
-                  href="https://mw-starwars-api.herokuapp.com/"
+                  href="https://react-todo-mw.herokuapp.com/"
                   target="_blank"
                 >
                   SEE LIVE
                 </UrlButton>
                 <UrlButton
-                  href="https://github.com/mwardak/Star-Wars-API"
+                  href="https://github.com/mwardak/React-ToDo"
                   target="_blank"
                 >
                   SOURCE CODE
@@ -286,12 +295,96 @@ const TimeLine = () => {
             </div>
           </ImageEvent>
 
-          {/* Project: Calculator */}
+          {/* Project: Expense Tracker */}
+          <ImageEvent
+            className="text-center"
+            text="Expense Tracker"
+            src={L_ExpenseTracker}
+            alt="Expense Tracker"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> This app was designed to
+                        track all expenses and help improve spending habits.
+                        Keeping a daily record of your expenses improves the
+                        financial health of your budget.
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>Add detailed transactions</li>
+                          <li>Remove transactions</li>
+                          <li>Responsive Design</li>
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="HTML 5"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              HTML5
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_BOOTSTRAP4}
+                                alt="CSS 3"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              CSS3
+                            </span>
+                          </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://expense-tracker-maher.herokuapp.com/"
+                  target="_blank"
+                >
+                  SEE LIVE
+                </UrlButton>
+                <UrlButton
+                  href="https://github.com/mwardak/Expense-Tracker"
+                  target="_blank"
+                >
+                  SOURCE CODE
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+
+          {/* Calculator */}
+
           <ImageEvent
             className="text-center"
             text="Calculator"
             src={L_Calculator}
             alt="Calculator"
+            //className to size the image
+
+            className="img-fluid"
           >
             <div className="d-flex justify-content-between flex-column mt-1">
               <div>
@@ -347,9 +440,9 @@ const TimeLine = () => {
                             <span className="p-2">
                               <Image
                                 src={L_JAVASCRIPT}
-                                alt="React"
+                                alt="Bootstrap 4"
                                 rounded
-                                className="image-style1 m-1"
+                                className="image-style m-1"
                               ></Image>{" "}
                               Javascript
                             </span>
@@ -368,94 +461,9 @@ const TimeLine = () => {
                   SEE LIVE
                 </UrlButton>
                 <UrlButton
-                  href="https://github.com/mwardak/Calculator-App"
+                  href="https://github.com/mwardak/Calculator-App "
                   target="_blank"
                 >
-                  SOURCE CODE
-                </UrlButton>
-              </div>
-            </div>
-          </ImageEvent>
-
-          {/* Project: To Do List */}
-
-          <ImageEvent
-            className="text-center"
-            text="To Do List"
-            src={L_JAVASCRIPT}
-            alt="To Do List"
-          >
-            <div className="d-flex justify-content-between flex-column mt-1">
-              <div>
-                <Accordion>
-                  <Card>
-                    <Accordion.Toggle
-                      as={Card.Header}
-                      eventKey="0"
-                      className="p-2 text-center accordian-main"
-                    >
-                      PROJECT DETAILS
-                    </Accordion.Toggle>
-
-                    <Accordion.Collapse eventKey="0" className="text-left">
-                      <Card.Body>
-                        <strong>Description:</strong> This application tracks
-                        your list of to do items so you can stay organized
-                        <hr />
-                        <strong>Features:</strong>
-                        <ul className="list-styles pt-1">
-                          <li>Add new to do</li>
-                          <li>Check box for completed</li>
-                          <li>Delet to do</li>
-                          <li>Clear entire list</li>
-                        </ul>
-                        <hr />
-                        <strong>Tech used:</strong>
-                        <ul>
-                          <li>
-                            <span className="p-2">
-                              <Image
-                                src={L_HTML5}
-                                alt="HTML 5"
-                                rounded
-                                className="image-style m-1"
-                              ></Image>{" "}
-                              HTML5
-                            </span>
-                          </li>
-                          <li>
-                            <span className="p-2">
-                              <Image
-                                src={L_CSS3}
-                                alt="CSS 3"
-                                rounded
-                                className="image-style m-1"
-                              ></Image>{" "}
-                              CSS3
-                            </span>
-                          </li>
-                          <li>
-                            <span className="p-2">
-                              <Image
-                                src={L_JAVASCRIPT}
-                                alt="Bootstrap 4"
-                                rounded
-                                className="image-style m-1"
-                              ></Image>{" "}
-                              Javascript
-                            </span>
-                          </li>
-                        </ul>
-                      </Card.Body>
-                    </Accordion.Collapse>
-                  </Card>
-                </Accordion>
-              </div>
-              <div className="d-flex justify-content-between flex-nowrap text-center">
-                <UrlButton href="" target="_blank">
-                  SEE LIVE
-                </UrlButton>
-                <UrlButton href="" target="_blank">
                   SOURCE CODE
                 </UrlButton>
               </div>
