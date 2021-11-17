410ceff6fe24570a02d08522d4b45fc44669613a
Maher Wardak <maher.wardak@gmail.com>
2021-11-16 23:09:27 -0500

    updated

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -12,13 +12,18 @@ const About = () => {
   return (
     <div id="about">
       <div className="about">
-        
         <Container>
-        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+          <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
           <Row className="pt-3 pb-5 align-items-center">
             <Col xs={12} md={6}>
               <Row className="justify-content-center mb-2 mr-2 ">
-                <Image className="profile justify-content-end" alt="profile" src={maher}  thumbnail fluid />
+                <Image
+                  className="profile justify-content-end"
+                  alt="profile"
+                  src={maher}
+                  thumbnail
+                  fluid
+                />
               </Row>
             </Col>
             <Col xs={12} md={6}>
@@ -26,18 +31,24 @@ const About = () => {
                 Hi there! I am <strong>&nbsp;Maher Wardak</strong>
                 <br /> I am a self-taught Full Stack Developer .
                 <br />
-                 I started teaching myself code 2 years ago and it has been a great journey learning how web applications are developed.   
+                In 2019 I joined The Software Development Mastermind Program and
+                learned everything from HTML, CSS, JavaScript, to building web
+                application in React, Node.js, Express, PostgreSQL.
                 <br />
-                 Front-end is where I began but was quickly drawn into full-stack because I wanted to understand how an application is built in its entirety. 
-                
-                 Fast-forward to present day, I built several applications and have broadened my knowlege of web applications. 
-                 
-                 The sense of fulfillment that I get when I solve a problem or create a new web app is unmatched and has propelled me to continue building.
-                 I hope to one day become a valuable contributor to a team that emphasizes teamwork, as well as individual growth.
                 <br />
- 
+                Front-end is where I began but was quickly drawn into full-stack
+                because I wanted to understand how an application is built in
+                its entirety. Fast-forward to present day, I built several
+                applications and have broadened my knowlege of web applications.
                 <br />
-
+                <br />
+                The sense of fulfillment that I get when I solve a problem or
+                create a new web app is unmatched and has propelled me to
+                continue building. I hope to one day become a valuable
+                contributor to a team that emphasizes teamwork, as well as
+                individual growth.
+                <br />
+                <br />
                 <Col className="d-flex justify-content-center flex-wrap">
                   <div>
                     <a href="#contact">
@@ -47,21 +58,33 @@ const About = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="" target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="https://drive.google.com/file/d/15imzlFogzUUZsPAvLq2OHPTH6lmMN-78/view?usp=sharing"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
                     </a>
                   </div>
                   <div>
-                    <a href="https://github.com/mwardak" target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="https://github.com/mwardak"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <Button className="m-2" variant="outline-dark">
                         GitHub
                       </Button>
                     </a>
                   </div>
                   <div>
-                    <a href="https://www.linkedin.com/in/maherwardak/" target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="https://www.linkedin.com/in/maherwardak/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <Button className="m-2" variant="outline-info">
                         LinkedIn
                       </Button>
--- src/pages/skills/skills.component.jsx
+++ src/pages/skills/skills.component.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
@@ -15,20 +14,33 @@ const Skills = () => {
   return (
     <div className="pt-3 pb-3" id="skills">
       <h1 className="text-center font-details-b pb-4">TECH SKILLS</h1>
-      <CardDeck>
+      <CardDeck style={{ justifyContent: "center" }}>
         <Row className="d-flex justify-content-around">
           {/* Frontend */}
           <Col md={4}>
             <Card className="focus mt-2 mb-2">
               <Card.Body>
                 {/* Frontend */}
-                <Card.Title className="text-center  card-title">Frontend</Card.Title>
+                <Card.Title className="text-center  card-title">
+                  Frontend
+                </Card.Title>
                 <hr />
                 <Card.Text className="card-text d-flex justify-content-start flex-column">
                   {skills.frontend.map((skill, index) => (
                     <span className="p-2" key={index}>
-                      <a className="text-dark text-decoration-none" href={skill.link} target="_blank" rel="noopener noreferrer">
-                        <Image src={skill.imgSrc} alt={skill.imgAltText} rounded className="image-style m-1"></Image> {skill.skillName}
+                      <a
+                        className="text-dark text-decoration-none"
+                        href={skill.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Image
+                          src={skill.imgSrc}
+                          alt={skill.imgAltText}
+                          rounded
+                          className="image-style m-1"
+                        ></Image>{" "}
+                        {skill.skillName}
                       </a>
                     </span>
                   ))}
@@ -41,13 +53,26 @@ const Skills = () => {
           <Col md={4}>
             <Card className="focus mt-2 mb-2">
               <Card.Body>
-                <Card.Title className="text-center  card-title">Backend</Card.Title>
+                <Card.Title className="text-center  card-title">
+                  Backend
+                </Card.Title>
                 <hr />
                 <Card.Text className="card-text d-flex justify-content-start flex-column">
                   {skills.backend.map((skill, index) => (
                     <span className="p-2" key={index}>
-                      <a className="text-dark text-decoration-none" href={skill.link} target="_blank" rel="noopener noreferrer">
-                        <Image src={skill.imgSrc} alt={skill.imgAltText} rounded className="image-style m-1"></Image> {skill.skillName}
+                      <a
+                        className="text-dark text-decoration-none"
+                        href={skill.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Image
+                          src={skill.imgSrc}
+                          alt={skill.imgAltText}
+                          rounded
+                          className="image-style m-1"
+                        ></Image>{" "}
+                        {skill.skillName}
                       </a>
                     </span>
                   ))}
@@ -58,13 +83,26 @@ const Skills = () => {
             {/* Hosting Platforms */}
             <Card className="focus mt-2 mb-2 ">
               <Card.Body>
-                <Card.Title className="text-center  card-title">Hosting Platforms</Card.Title>
+                <Card.Title className="text-center  card-title">
+                  Hosting Platforms
+                </Card.Title>
                 <hr />
                 <Card.Text className="card-text d-flex justify-content-start flex-column">
                   {skills.hostingPlatforms.map((skill, index) => (
                     <span className="p-2" key={index}>
-                      <a className="text-dark text-decoration-none" href={skill.link} target="_blank" rel="noopener noreferrer">
-                        <Image src={skill.imgSrc} alt={skill.imgAltText} rounded className="image-style m-1"></Image> {skill.skillName}
+                      <a
+                        className="text-dark text-decoration-none"
+                        href={skill.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Image
+                          src={skill.imgSrc}
+                          alt={skill.imgAltText}
+                          rounded
+                          className="image-style m-1"
+                        ></Image>{" "}
+                        {skill.skillName}
                       </a>
                     </span>
                   ))}
@@ -75,19 +113,30 @@ const Skills = () => {
 
           {/* Programming Languages */}
           <Col md={4}>
-           
-
             {/* Database */}
 
             <Card className="focus mt-2 mb-2">
               <Card.Body>
-                <Card.Title className="text-center  card-title">Database</Card.Title>
+                <Card.Title className="text-center  card-title">
+                  Database
+                </Card.Title>
                 <hr />
                 <Card.Text className="card-text d-flex justify-content-start flex-column">
                   {skills.databases.map((skill, index) => (
                     <span className="p-2" key={index}>
-                      <a className="text-dark text-decoration-none" href={skill.link} target="_blank" rel="noopener noreferrer">
-                        <Image src={skill.imgSrc} alt={skill.imgAltText} rounded className="image-style m-1"></Image> {skill.skillName}
+                      <a
+                        className="text-dark text-decoration-none"
+                        href={skill.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Image
+                          src={skill.imgSrc}
+                          alt={skill.imgAltText}
+                          rounded
+                          className="image-style m-1"
+                        ></Image>{" "}
+                        {skill.skillName}
                       </a>
                     </span>
                   ))}
@@ -98,12 +147,25 @@ const Skills = () => {
 
             <Card className="focus mt-2 mb-2">
               <Card.Body>
-                <Card.Title className="text-center  card-title">Version Control</Card.Title>
+                <Card.Title className="text-center  card-title">
+                  Version Control
+                </Card.Title>
                 <hr />
                 <Card.Text className="card-text d-flex justify-content-start flex-column">
                   <span className="p-2">
-                    <a className="text-dark text-decoration-none" href={skills.versionControl[0].link} target="_blank" rel="noopener noreferrer">
-                      <Image src={skills.versionControl[0].imgSrc} alt={skills.versionControl[0].imgAltText} rounded className="image-style m-1"></Image> {skills.versionControl[0].skillName}
+                    <a
+                      className="text-dark text-decoration-none"
+                      href={skills.versionControl[0].link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src={skills.versionControl[0].imgSrc}
+                        alt={skills.versionControl[0].imgAltText}
+                        rounded
+                        className="image-style m-1"
+                      ></Image>{" "}
+                      {skills.versionControl[0].skillName}
                     </a>
                   </span>
                 </Card.Text>
